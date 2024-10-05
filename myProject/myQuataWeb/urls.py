44180aff686c4957d7db8868208c60033e13c7e3
6510685016 (Kasidit Kornsang) <110814176+6510685016@users.noreply.github.com>
2024-10-05 23:37:00 +0700

    fix ui

--- myProject/myQuataWeb/urls.py
+++ myProject/myQuataWeb/urls.py
@@ -2,18 +2,12 @@ from django.urls import path
 from myQuataWeb import views
 
 urlpatterns = [
-    path('', views.home),
-    path('myQuota', views.myQuota),
-    path('findSub', views.findSub),
-    path('register', views.register),
-    path('login', views.login),
-    
-    path('add-quota-request/', views.add_quota_request, name='add_quota_request'),
-    path('cancel-quota-request/<int:student_id>/<int:subject_id>/', views.cancel_quota_request, name='cancel_quota_request'),
     path('', views.greeting, name='greeting'),
     path('home', views.home, name='home'),
     path('myQuota', views.myQuota, name='myQuota'),
     path('findSub', views.findSub, name='findSub'),
-
+    
+    path('add-quota-request/', views.add_quota_request, name='add_quota_request'),
+    path('cancel-quota-request/<int:student_id>/<int:subject_id>/', views.cancel_quota_request, name='cancel_quota_request'),
 ]
 
