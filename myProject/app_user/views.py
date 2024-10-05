02cd2b5b7a623516cc9e2a80e6e6cca80c69eeee
Folkchayutpong <folkchayutpong@gmail.com>
2024-10-05 10:38:43 +0700

    fix reg

--- myProject/app_user/views.py
+++ myProject/app_user/views.py
@@ -7,26 +7,37 @@ from myQuataWeb.models import Student
 def register(request):
     if request.method == "POST":
         user_form = UserRegisterForm(request.POST)
-        student_form = StudentForm(request.POST, request.FILES) 
+        student_form = StudentForm(request.POST, request.FILES)  
 
         if user_form.is_valid() and student_form.is_valid():
             user = user_form.save()  
             login(request, user)  
-                
+
+            profile_pic = student_form.cleaned_data.get('profile_pic') 
+            if not profile_pic:  # ถ้าไม่มีการอัปโหลดไฟล์
+                profile_pic = 'profile_photos/default.jpg'
+
+            # บันทึกข้อมูลนักเรียน
             student = Student(
                 first_name=student_form.cleaned_data['first_name'],
                 last_name=student_form.cleaned_data['last_name'],
                 stu_id=user.username,
                 faculty=student_form.cleaned_data['faculty'],
-                profile_pic=student_form.cleaned_data['profile_pic'],
+                profile_pic=profile_pic, 
             )
-            student.save()  
-            
+
+            print(profile_pic)
+
+            try:
+                student.save() 
+            except Exception as e:
+                print(f"Error saving student: {e}")  
+
             return redirect('home')
     else:
         user_form = UserRegisterForm()
         student_form = StudentForm()
-    
+
     context = {
         "user_form": user_form,
         "student_form": student_form,
